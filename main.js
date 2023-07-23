57b5cbd319e611c34dab0e2b589339b82c389d41
Rakshan <rakshanshetty2003@gmail.com>
2023-07-23 21:07:18 +0530

    save

---
+++ main.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+var typed = new Typed(".text",{
+    strings: ["Full Stack Web Developer","Programmer"],
+    typeSpeed:80,
+    backSpeed:70,
+    backDelay:100,
+    loop:true
+});
+});
+
