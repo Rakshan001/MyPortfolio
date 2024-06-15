5bda6a1d4e7312656a3b7d42fac3a284e1cf635a
Rakshan001 <rakshanshetty2003@gmail.com>
2024-06-16 00:09:10 +0530

    resume added

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
 var typed = new Typed(".text",{
-    strings: ["Full Stack Web Developer","Programmer"],
+    strings: ["Full Stack Web Developer","Programmer"," AIML Engineer"],
     typeSpeed:80,
     backSpeed:70,
     backDelay:100,
@@ -8,7 +8,7 @@ var typed = new Typed(".text",{
 });
 
 var typed = new Typed(".text2",{
-    strings: ["Full Stack Web Developer"],
+    strings: ["Full Stack Web Developer"," AIML Engineer"],
     typeSpeed:50,
     backSpeed:70,
     backDelay:1000,
@@ -22,5 +22,26 @@ var typed = new Typed(".text2",{
 // toggleButton.addEventListener('click', ()=>{
 //     navbar.classList.toggle('active')
 // })
+
+
+function displayResume() {
+    // Replace with the path to your PDF file
+    const pdfPath = 'Rakshans_Resume.pdf';
+
+    // Create a link element
+    const link = document.createElement('a');
+    link.href = pdfPath;
+    link.target = '_blank'; // Open in a new tab
+    // link.setAttribute('download', ''); // Optional: Add this attribute to enable download
+
+    // Append the link to the body
+    document.body.appendChild(link);
+
+    // Trigger the click event on the link
+    link.click();
+
+    // Clean up: remove the link from the body
+    document.body.removeChild(link);
+}
 
 
